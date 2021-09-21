ffcb66a6bbe5a45a3341e31faa764a5aa51021b6
Dale Margel <dale.margel@gmail.com>
2021-09-20 19:24:18 -0600

    initial

---
+++ src/wc-frag.js
@@ -0,0 +1,27 @@
+let template = document.createElement('template');
+template.innerHTML = `<div></div>`;
+
+class WcFrag extends HTMLElement {
+	constructor() { super();
+		const shadowRoot = this.attachShadow({mode: 'open'});
+		shadowRoot.appendChild(template.content.cloneNode(true));
+	}
+	static get observedAttributes() { return ['href'] }
+
+	async attributeChangedCallback(name, oldVal, newVal) {
+		let body = '';
+		if(newVal && newVal != ''){
+			try {
+				const rslt = await fetch(newVal);
+				const text = await rslt.text();
+				const frag = /<body>(?<BODY>.*)<\/body>/ms.exec(text);
+				body = frag.groups.BODY;
+			} catch {}
+		}
+		const div = this.shadowRoot.querySelector('div');
+		div.innerHTML = body;
+	}
+}
+try{ customElements.define("wc-frag",WcFrag) }
+catch(NotSupportedError){/* duplicate */}
+export { WcFrag }
